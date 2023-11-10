f1055c02f4010caf589bb3330570dfc84ef36462
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-11-10 20:35:16 +0530

    module updated

--- main.tf
+++ main.tf
@@ -3,4 +3,13 @@ resource "azurerm_lb_backend_address_pool" "lb_backend_pool" {
   name = each.value.name
   loadbalancer_id = var.load_balancer_output[each.value.loadbalancer_name].id
   virtual_network_id = var.virtual_network_output[each.value.virtual_network_name].id
+  dynamic "tunnel_interface" {
+    for_each = each.value.tunnel_interface == [] ? [] : [{}]
+    content {
+      identifier = tunnel_interface.value.identifier
+      type       = tunnel_interface.value.type
+      protocol   = tunnel_interface.value.protocol
+      port       = tunnel_interface.value.port
+    }
+  }
 }
