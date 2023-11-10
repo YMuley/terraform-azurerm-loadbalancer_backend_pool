9d31802f5723c48ab6597734dbea5b59a09bfd54
Yogesh Muley <Yogesh.Muley@MOTIFWORKS.IN>
2023-11-10 21:39:05 +0530

    first commit

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ resource "azurerm_lb_backend_address_pool" "lb_backend_pool" {
   loadbalancer_id = var.load_balancer_output[each.value.loadbalancer_name].id
   virtual_network_id = var.virtual_network_output[each.value.virtual_network_name].id
   dynamic "tunnel_interface" {
-    for_each = lenght(each.value.tunnel_interface) == 0 ? [] : [each.value.tunnel_interface]
+    for_each = length(each.value.tunnel_interface) == 0 ? [] : [each.value.tunnel_interface]
     content {
       identifier = tunnel_interface.value.identifier
       type       = tunnel_interface.value.type
